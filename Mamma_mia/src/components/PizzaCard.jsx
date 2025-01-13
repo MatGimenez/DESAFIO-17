9ace03d58df91b55791b720a0016a0b44a10e17e
Matias Gimenez <p.matias.gimenez@hotmail.es>
2025-01-13 11:08:57 -0300

    uso de comas en ingredientes de pizza card

--- Mamma_mia/src/components/PizzaCard.jsx
+++ Mamma_mia/src/components/PizzaCard.jsx
@@ -1,6 +1,7 @@
 
 import Card from "react-bootstrap/Card";
 import ListGroup from 'react-bootstrap/ListGroup';
+import { pizzas } from "../assets/pizzas";
 
 const PizzaCard = ({ img, name, ingredients, price }) => {
     return (
@@ -11,11 +12,13 @@ const PizzaCard = ({ img, name, ingredients, price }) => {
                     <ListGroup.Item><Card.Title>Pizza {name}</Card.Title></ListGroup.Item>
                     <ListGroup.Item style={{ textAlign: "center" }}>
                         <Card.Text>Ingredientes:</Card.Text>
-                        <Card.Text style={{ fontSize: "14px", color: "gray", display:"flex" , justifyContent:"center"}}>🍕 
+                        <Card.Text style={{ fontSize: "14px", color: "gray", display: "flex", justifyContent: "center" }}>🍕
                             {
-                                ingredients.map(ingred => ingred + ", ")
+                                ingredients.map((ingred, recorrido) => (
+                                    <span key={ingred}>{ingred}{recorrido < ingredients.length - 1 && ", "}</span>
+                                ))
                             }
-                            </Card.Text>
+                        </Card.Text>
                     </ListGroup.Item>
                     <ListGroup.Item style={{ textAlign: "center" }}>
                         <Card.Text style={{ fontSize: "1.2rem", fontWeight: "500" }}>Precio: ${price.toLocaleString("es-CL")}</Card.Text>
